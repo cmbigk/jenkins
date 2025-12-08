b57af7d1e4244e80c667b0099282eef97f289aef
chmyint <bigk89865@gmail.com>
2025-12-09 01:57:30 +0200

    changed the backend to https to be more secure

--- product-service/src/main/java/com/ecommerce/productservice/config/WebClientConfig.java
+++ product-service/src/main/java/com/ecommerce/productservice/config/WebClientConfig.java
@@ -1,14 +1,36 @@
 package com.ecommerce.productservice.config;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
 
 @Configuration
 public class WebClientConfig {
     
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        try {
+            // Configure SSL context to accept self-signed certificates
+            SslContext sslContext = SslContextBuilder
+                    .forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+            
+            HttpClient httpClient = HttpClient.create()
+                    .secure(sslSpec -> sslSpec.sslContext(sslContext));
+            
+            return WebClient.builder()
+                    .clientConnector(new ReactorClientHttpConnector(httpClient));
+        } catch (SSLException e) {
+            // Fallback to default if SSL configuration fails
+            return WebClient.builder();
+        }
     }
 }
