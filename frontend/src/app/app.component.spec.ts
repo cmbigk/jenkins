31b1270120eef8bb84469af31124a63c0d29e22b
chmyint <bigk89865@gmail.com>
2026-01-19 23:37:00 +0200

    failing test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -21,7 +21,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect().toBeTruthy();
   });
 
   it('should have authService injected', () => {
